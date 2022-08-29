ad9fae49af98bd445ba7d57f3c399382425ada69
Kohkj <87573761+Kohkj@users.noreply.github.com>
2022-08-29 11:48:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+customhost = "employeedb.cbgc3gnvgfld.us-east-1.rds.amazonaws.com"
+
+customuser = "aws_user"
+
+custompass = "bait3273"
+
+customdb = "employee"
+
+custombucket = "kkj-employee"
+
+customregion = "us-east-1"
